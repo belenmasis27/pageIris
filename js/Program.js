18abce5d329caabb93abe8ba80f1ee52be4be118
belenmasis27 <irisbelen3237@gmail.com>
2019-12-08 10:41:25 -0600

    pageIris

---
+++ js/Program.js
@@ -0,0 +1,17 @@
+var doc = new jsPDF();
+var elementHTML = $('#content').html();
+var specialElementHandlers = {
+    '#elementH': function (element, renderer) {
+        return true;
+    }
+};
+doc.fromHTML(elementHTML, 15, 15, {
+    'width': 180,
+    'elementHandlers': specialElementHandlers
+});
+
+doc.save('sample-document.pdf');
+
+
+
+
